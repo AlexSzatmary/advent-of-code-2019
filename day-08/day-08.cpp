c072f8a41c9a0092d71807cce8ea4b9d640f55a8
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-04-07 14:55:08 -0400

    Solve day-08-1

---
+++ day-08/day-08.cpp
@@ -0,0 +1,63 @@
+#include "cxxopts.hpp"
+#include <algorithm>
+#include <list>
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+using BodyGraph = map<string, list<string>>;
+
+auto const WIDTH{25};
+auto const HEIGHT{6};
+auto const LAYER_SIZE{WIDTH * HEIGHT};
+
+vector<string> my_parse(ifstream &inf){
+    string line;
+    getline(inf, line);
+    vector<string> layers{};
+    for (int i{0}; i + LAYER_SIZE <= line.size(); i += LAYER_SIZE){
+        layers.push_back(line.substr(i, LAYER_SIZE));
+    }
+    return layers;
+}
+
+string get_least_corrupted_layer(vector<string> layers){
+    return *min_element(begin(layers), end(layers),
+    [](string sa, string sb){
+        return count(begin(sa), end(sa), '0') < count(begin(sb), end(sb), '0');
+    });
+}
+
+int count_1_by_count_2(string layer){
+    return count(begin(layer), end(layer), '1') * count(begin(layer), end(layer), '2');
+}
+
+int main(int argv, char **argc){
+    cxxopts::Options options("test", "A brief description");
+    options.add_options()
+        ("1", "Solve part 1", cxxopts::value<bool>())
+        ("2", "Solve part 2", cxxopts::value<bool>())
+        ("h,help", "Print usage");
+    auto result = options.parse(argv, argc);
+
+   if (result.count("help"))
+    {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+    if (result.count("1")) {
+        ifstream inf{result.unmatched()[0]};
+        auto layers{my_parse(inf)};
+        string least_corrupted = get_least_corrupted_layer(layers);
+        cout << count_1_by_count_2(least_corrupted) << endl;
+    }
+    if (result.count("2")) {
+        ifstream inf{result.unmatched()[0]};
+    }
+    return 0;
+}
