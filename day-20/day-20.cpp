e613ec9426c7d94f975134b7cbd1ebeec8d68ada
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-11-02 21:09:21 -0400

    Solve day-20-2

--- day-20/day-20.cpp
+++ day-20/day-20.cpp
@@ -26,10 +26,20 @@ struct Coord {
   int j{};
 };
 
+struct Coord3D {
+  int i{};
+  int j{};
+  int k{};  // level
+};
+
 using WarpMap = unordered_map<string, pair<Coord, Coord>>;
 
 inline bool operator<(const Coord& lhs, const Coord& rhs) {
   return tie(lhs.i, lhs.j) < tie(rhs.i, rhs.j);
+}
+
+inline bool operator<(const Coord3D& lhs, const Coord3D& rhs) {
+  return tie(lhs.i, lhs.j, lhs.k) < tie(rhs.i, rhs.j, rhs.k);
 }
 
 string find_portal_at_location(vector<string> maze, int i, int j) {
@@ -133,6 +143,76 @@ int flood_fill_maze(vector<string> maze, WarpMap warp_map) {
   return dist;
 }
 
+bool is_outer(vector<string>& maze, int i, int j) {
+  return i < 2 || i >= maze.size() - 2 || j < 2 || j >= maze.at(0).size() - 4;
+}
+
+int flood_fill_maze_3D(vector<string> maze, WarpMap warp_map) {
+  int dist{0};
+  Coord start{crawl_for_a_real_spot(maze, warp_map["AA"].first)};
+  list<Coord3D> now_edges{Coord3D{start.i, start.j, 0}};
+  list<Coord3D> next_edges{};
+  set<Coord3D> processed{};
+  while (dist < 10000 && now_edges.size() > 0) {
+    // cout << endl;
+    // cout << "dist " << dist << endl;
+    // vector<string> maze_image{maze};
+    // for (auto edge: now_edges){
+    //   maze_image.at(edge.i).at(edge.j) = '0' + edge.k % 10;
+    // }
+    // for (auto s: maze_image) cout << s << endl;
+
+    for (Coord3D step : now_edges) {
+      for (auto [di, dj] :
+           vector({pair(-1, 0), pair(1, 0), pair(0, -1), pair(0, 1)})) {
+        Coord3D next_step{step.i + di, step.j + dj, step.k};
+        char here{maze.at(next_step.i).at(next_step.j)};
+        if (here == '#') continue;
+        if (processed.find(next_step) != processed.end()) continue;
+        if (here != '.') {
+          processed.insert(next_step);
+          string portal{
+              find_portal_at_location(maze, next_step.i, next_step.j)};
+          if (portal == "AA") continue;
+          if (portal == "ZZ") {
+            if (next_step.k == 0)
+              return dist;
+            else
+              continue;
+          }
+          if (is_outer(maze, next_step.i, next_step.j)) {
+            if (next_step.k == 0) continue;
+            --next_step.k;
+          } else
+            ++next_step.k;
+
+          Coord jumped_step{};
+          auto node{warp_map[portal]};
+          if (node.first.i == next_step.i && node.first.j == next_step.j) {
+            jumped_step = node.second;
+          } else
+            jumped_step = node.first;
+
+          next_step.i = jumped_step.i;
+          next_step.j = jumped_step.j;
+          processed.insert(next_step);
+
+          auto next_step_2D = crawl_for_a_real_spot(maze, jumped_step);
+          next_step.i = next_step_2D.i;
+          next_step.j = next_step_2D.j;
+        }
+        next_edges.push_back(next_step);
+      }
+      processed.insert(step);
+    }
+    now_edges.clear();
+    now_edges.splice(now_edges.end(), next_edges);
+    next_edges.clear();
+    ++dist;
+  }
+  return dist;
+}
+
 int main(int argv, char** argc) {
   cxxopts::Options options("test", "A brief description");
   options.add_options()("1", "Solve part 1", cxxopts::value<bool>())(
@@ -151,6 +231,10 @@ int main(int argv, char** argc) {
     cout << flood_fill_maze(maze, warp_map) << endl;
   }
   if (result.count("2")) {
+    ifstream inf{result.unmatched()[0]};
+    auto maze = my_parse(inf);
+    auto warp_map = make_warp_map(maze);
+    cout << flood_fill_maze_3D(maze, warp_map) << endl;
   }
   return 0;
 }
